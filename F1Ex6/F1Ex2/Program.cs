61c01d52f8e6ff23748be2003f8c7609c26a7a8b
Nuno Rocha <intersect.nerd@gmail.com>
2018-10-02 13:19:25 +0100

    Aula 2/10/2018
    Ficha 1 exercicio 6

---
+++ F1Ex6/F1Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public List<Tarefa> ListarTarefas(List<Tarefa> list)
+        {
+            list.OrderBy(o => o.Prioridade).ThenBy(o => o.Indice).ToList();
+            return list;
+        }
+
+        public List<Tarefa> tarefasEmAtraso(List<Tarefa> list)
+        {
+            Func<Tarefa, bool> isEmAtraso = t => t.DataLimite < DateTime.Today && t.Estado != Estado.Concluida;
+            List<Tarefa> ListadetarefasEmAtraso = list.Where(isEmAtraso).ToList<Tarefa>();
+            return ListadetarefasEmAtraso;
+        }
+
+        public void apagarTarefasConcluidas(List<Tarefa> list)
+        {
+            list.RemoveAll(x => x.Estado == Estado.Concluida);
+        }
+
+    }
+}
---
+++ F1Ex6/F1Ex2/Tarefa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Ex2
+{
+    enum Prioridade { Baixa, Normal, Alta}
+    enum Categoria { Pessoal, trabalho }
+    enum Estado { Porexecutar, emExecucao, Concluida}
+    class Tarefa
+    {
+        private string titulo;
+        private DateTime dataLimite;
+        private static int nindice = 0;
+        private int indice;
+
+        public Tarefa(string titulo, DateTime dataLimite, Prioridade prioridade = Prioridade.Alta, Estado estado = Estado.Porexecutar, Categoria categoria = Categoria.trabalho)
+        {
+            Prioridade = prioridade;
+            Estado = estado;
+            Categoria = categoria;
+
+            Titulo = titulo;
+            DataLimite = dataLimite;
+
+            nindice++;
+            Indice = nindice;
+            
+
+        }
+
+        public Prioridade Prioridade { get; set; }
+        public Estado Estado { get; set; }
+        public Categoria Categoria { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLimite { get; set; }
+        public int Indice { get; set; }
+
+    }
+}
